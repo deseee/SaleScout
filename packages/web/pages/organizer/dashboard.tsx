3959513b9f75ce0d3ec241254bb83f25a3121f0e
Deseee <deseee@gmail.com>
2026-02-22 19:22:30 -0500

    style: reduce icon size in organizer dashboard

--- packages/web/pages/organizer/dashboard.tsx
+++ packages/web/pages/organizer/dashboard.tsx
@@ -161,7 +161,7 @@ export default function Dashboard() {
             <div className="px-4 py-5 sm:p-6">
               <div className="flex items-center">
                 <div className="flex-shrink-0 bg-blue-100 rounded-md p-1">
-                  <svg className="h-4 w-4 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <svg className="h-1 w-1 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
                   </svg>
                 </div>
